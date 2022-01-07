796e820f269b29faf8a80344e94525cb85daaae6
jamahun <jackmansfieldhung@gmail.com>
2022-01-07 12:45:02 +1100

    Update starry_stanford.sh

--- examples/scripts/starry_stanford.sh
+++ examples/scripts/starry_stanford.sh
@@ -3,7 +3,8 @@
 # wget -c https://upload.wikimedia.org/wikipedia/commons/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg -O starry_night_gigapixel.jpg
 # Or you can manually download the image from here: https://commons.wikimedia.org/wiki/File:Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
 
-STYLE_IMAGE=starry_night_gigapixel.jpg
+
+
 CONTENT_IMAGE=examples/inputs/hoovertowernight.jpg
 
 STYLE_WEIGHT=5e2
@@ -12,81 +13,84 @@ STYLE_SCALE=1.0
 STYLE_WEIGHT2=2500 # Style weight for image size 2048 and above
 
 PYTHON=python3 # Change to Python if using Python 2
-SCRIPT=neural_style.py
+BATCH_SCRIPT=./examples/scripts/batch.py
+NS_SCRIPT=neural_style.py
 GPU=0
 
 NEURAL_STYLE=$PYTHON
 NEURAL_STYLE+=" "
-NEURAL_STYLE+=$SCRIPT
+NEURAL_STYLE+=$NS_SCRIPT
 
-# Uncomment if using pip package
-#NEURAL_STYLE=neural-style
+BATCH=$PYTHON
+BATCH+=" "
+BATCH+=$BATCH_SCRIPT
 
+$BATCH \
+ STYLE_IMAGE=
+  $NEURAL_STYLE \
+    -content_image $CONTENT_IMAGE \
+    -style_image $STYLE_IMAGE \
+    -style_scale $STYLE_SCALE \
+    -print_iter 1 \
+    -style_weight $STYLE_WEIGHT \
+    -image_size 256 \
+    -output_image out1.png \
+    -tv_weight 0 \
+    -gpu $GPU \
+    -backend cudnn -cudnn_autotune
 
-$NEURAL_STYLE \
-  -content_image $CONTENT_IMAGE \
-  -style_image $STYLE_IMAGE \
-  -style_scale $STYLE_SCALE \
-  -print_iter 1 \
-  -style_weight $STYLE_WEIGHT \
-  -image_size 256 \
-  -output_image out1.png \
-  -tv_weight 0 \
-  -gpu $GPU \
-  -backend cudnn -cudnn_autotune
+  $NEURAL_STYLE \
+    -content_image $CONTENT_IMAGE \
+    -style_image $STYLE_IMAGE \
+    -init image -init_image out1.png \
+    -style_scale $STYLE_SCALE \
+    -print_iter 1 \
+    -style_weight $STYLE_WEIGHT \
+    -image_size 512 \
+    -num_iterations 500 \
+    -output_image out2.png \
+    -tv_weight 0 \
+    -gpu $GPU \
+    -backend cudnn -cudnn_autotune
 
-$NEURAL_STYLE \
-  -content_image $CONTENT_IMAGE \
-  -style_image $STYLE_IMAGE \
-  -init image -init_image out1.png \
-  -style_scale $STYLE_SCALE \
-  -print_iter 1 \
-  -style_weight $STYLE_WEIGHT \
-  -image_size 512 \
-  -num_iterations 500 \
-  -output_image out2.png \
-  -tv_weight 0 \
-  -gpu $GPU \
-  -backend cudnn -cudnn_autotune
+  $NEURAL_STYLE \
+    -content_image $CONTENT_IMAGE \
+    -style_image $STYLE_IMAGE \
+    -init image -init_image out2.png \
+    -style_scale $STYLE_SCALE \
+    -print_iter 1 \
+    -style_weight $STYLE_WEIGHT \
+    -image_size 1024 \
+    -num_iterations 200 \
+    -output_image out3.png \
+    -tv_weight 0 \
+    -gpu $GPU \
+    -backend cudnn -cudnn_autotune
 
-$NEURAL_STYLE \
-  -content_image $CONTENT_IMAGE \
-  -style_image $STYLE_IMAGE \
-  -init image -init_image out2.png \
-  -style_scale $STYLE_SCALE \
-  -print_iter 1 \
-  -style_weight $STYLE_WEIGHT \
-  -image_size 1024 \
-  -num_iterations 200 \
-  -output_image out3.png \
-  -tv_weight 0 \
-  -gpu $GPU \
-  -backend cudnn -cudnn_autotune
+  $NEURAL_STYLE \
+    -content_image $CONTENT_IMAGE \
+    -style_image $STYLE_IMAGE \
+    -init image -init_image out3.png \
+    -style_scale $STYLE_SCALE \
+    -print_iter 1 \
+    -style_weight $STYLE_WEIGHT2 \
+    -image_size 2048 \
+    -num_iterations 200 \
+    -output_image out4.png \
+    -tv_weight 0 \
+    -gpu $GPU \
+    -backend cudnn
 
-$NEURAL_STYLE \
-  -content_image $CONTENT_IMAGE \
-  -style_image $STYLE_IMAGE \
-  -init image -init_image out3.png \
-  -style_scale $STYLE_SCALE \
-  -print_iter 1 \
-  -style_weight $STYLE_WEIGHT2 \
-  -image_size 2048 \
-  -num_iterations 200 \
-  -output_image out4.png \
-  -tv_weight 0 \
-  -gpu $GPU \
-  -backend cudnn
-
-$NEURAL_STYLE \
-  -content_image $CONTENT_IMAGE \
-  -style_image $STYLE_IMAGE \
-  -init image -init_image out4.png \
-  -style_scale $STYLE_SCALE \
-  -print_iter 1 \
-  -style_weight $STYLE_WEIGHT2 \
-  -image_size 2350 \
-  -num_iterations 200 \
-  -output_image out5.png \
-  -tv_weight 0 \
-  -gpu $GPU \
-  -backend cudnn -optimizer adam
+  $NEURAL_STYLE \
+    -content_image $CONTENT_IMAGE \
+    -style_image $STYLE_IMAGE \
+    -init image -init_image out4.png \
+    -style_scale $STYLE_SCALE \
+    -print_iter 1 \
+    -style_weight $STYLE_WEIGHT2 \
+    -image_size 2350 \
+    -num_iterations 200 \
+    -output_image out5.png \
+    -tv_weight 0 \
+    -gpu $GPU \
+    -backend cudnn -optimizer adam
